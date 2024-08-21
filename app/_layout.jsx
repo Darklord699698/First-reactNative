ea3273873db7e2b56d8de28866e002dadc410fb8
UJJWAL <ujjwal8851114020@gmail.com>
2024-08-22 02:28:33 +0530

    styles

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet,Text,View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot } from 'expo-router'
 
-export default function App(){
-  return(
-    <View style={StyleSheet.container}>
-      <Text>Aora</Text>
-      <StatusBar style="auto"/>
-    </View>
-  );
+const RootLayout = () => {
+  return (
+    <>
+    <Slot/>
+    </>
+  )
 }
 
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#fff',
-    alignItems:'center',
-    justifyContent:'center'
-  }
-})
+export default RootLayout
